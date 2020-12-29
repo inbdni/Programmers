8fdcc72d9262d5e0f63a2f1b448aa7d635877ecf
백지민 <jimin@baegjimin-ui-MacBookPro.local>
2020-12-29 10:51:47 +0900

    월간 코드 챌린지 시즌1

---
+++ level02/삼각달팽이.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    int i;
+    int x = 0;
+    int y = 0;
+    int num = 1;
+    int flag = 0;
+    int filled[1001][1001];
+    
+    while (flag < n)
+    {
+        i = flag;
+        if (flag % 3 == 0)
+        {
+            while (i++ < n)
+                filled[y++][x] = num++;
+            y--;
+            x++;
+        }
+        else if (flag % 3 == 1)
+        {
+            while (i++ < n)
+                filled[y][x++] = num++;
+            y--;
+            x -= 2;
+        }
+        else
+        {
+            while (i++ < n)
+                filled[y--][x--] = num++;
+            y += 2;
+            x++;
+        }
+        flag++;
+    }
+    for (y = 0; y < n; y++)
+    {
+        for (x = 0; x <= y; x++)
+            answer.push_back(filled[y][x]);
+    }
+    return answer;
+}
