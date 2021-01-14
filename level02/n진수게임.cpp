2c48aacb9ae27d923458a1b65f358135c71e0020
백지민 <jimin@baegjimin-ui-MacBookPro.local>
2021-01-14 23:55:05 +0900

    2018 KAKAO BLIND RECRUITMENT

---
+++ level02/n진수게임.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string base = "0123456789ABCDEF";
+
+string convert(int n, int num) {
+    string result = "";
+    
+    if (num == 0)
+    {
+        result += '0';
+        return result;
+    }
+    while (num)
+    {
+        result += base[num % n];
+        num /= n;
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+string solution(int n, int t, int m, int p) {
+    string answer = "";
+    int i;
+    int num = 0;
+    int seq = 0;
+    string baseNum;
+    
+    while (answer.size() < t)
+    {
+        baseNum += convert(n, num);
+        num++;
+        for (i = 0; i < baseNum.size(); i++)
+        {
+            seq++;
+            if (seq == p)
+                answer += baseNum[i];
+            if (seq == m)
+                seq = 0;
+            if (answer.size() == t)
+                break;
+        }
+        baseNum.clear();
+    }
+    return answer;
+}
