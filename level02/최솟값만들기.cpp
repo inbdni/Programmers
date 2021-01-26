e8d549920a5a50886edd8761451007e1b28f4cb9
백지민 <jimin@baegjimin-ui-MacBookPro.local>
2021-01-27 02:39:38 +0900

    연습문제

---
+++ level02/최솟값만들기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+    int i = 0;
+    
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    while (i < A.size()) {
+        answer += A[A.size() - i - 1] * B[i];
+        i++;
+    }
+
+    return answer;
+}
