46c5d8021553ea7ec42def706a6e2995e7f9d550
백지민 <jimin@baegjimin-ui-MacBookPro.local>
2021-02-15 02:13:13 +0900

    2017 카카오코드 예선

---
+++ level03/보행자천국.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <queue>
+#include <cstring>
+
+#define v   0
+#define h   1
+#define MOD 20170805
+
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> city_map) {
+    int answer;
+    int d[m][n][2];
+    int i, j, value, tmp;
+    
+    memset(d, 0, sizeof(d));
+    for (i = 0; i < m; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            if (i == 0 && j == 0)
+            {
+                d[i][j][v] = 1;
+                d[i][j][h] = 1;
+                continue;
+            }
+            value = city_map[i][j];
+            if (value == 0)
+            {
+                tmp = (i > 0) ? d[i - 1][j][v] : 0;
+                tmp += (j > 0) ? d[i][j - 1][h] : 0;
+                d[i][j][v] = tmp % MOD;
+                d[i][j][h] = tmp % MOD;
+            }
+            else if (value == 1)
+            {
+                d[i][j][v] = 0;
+                d[i][j][h] = 0;
+            }
+            else if (value == 2)
+            {
+                d[i][j][v] = (i > 0) ? d[i - 1][j][v] : 0;
+                d[i][j][h] = (j > 0) ? d[i][j - 1][h] : 0;
+            }
+        }
+    }
+    answer = d[m - 1][n - 1][v];
+    return answer;
+}
